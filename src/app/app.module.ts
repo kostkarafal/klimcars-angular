6fe925d41f20a1ca6fa8d8d6a4c2164df6557de1
Rafał Kostka (rafakos998) <rafakos998@student.polsl.pl>
2019-01-23 19:28:54 +0100

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LayoutComponent } from './layout/layout.component';
+import {RouterModule, Routes} from '@angular/router';
+import { AboutUsComponent } from './ui/about-us/about-us.component';
+import { MainComponent } from './ui/main/main.component';
+import { ContactComponent } from './ui/contact/contact.component';
+import { OfferComponent } from './ui/offer/offer.component';
+
+const appRoutes: Routes = [
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'main', component: MainComponent },
+  { path: 'offer', component: OfferComponent},
+  { path: 'contact', component: ContactComponent },
+  { path: '**', redirectTo: '/main', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LayoutComponent,
+    AboutUsComponent,
+    MainComponent,
+    ContactComponent,
+    OfferComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
